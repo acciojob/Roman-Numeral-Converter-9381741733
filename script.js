14b29bb520847731817d7e267f6d37353f986bc3
jyothsna bhukya <jyothsnabhukya022@gmail.com>
2025-03-16 16:51:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function convertToRoman(num) {
   if (num === 0) return ""; // No Roman numeral for 0
   
   const obj= [
-      ['M̅', 100000], ['C̅M̅', 90000], ['L̅', 50000], ['X̅L̅', 40000], 
-      ['X̅', 10000], ['I̅X̅', 9000], ['V̅', 5000], ['I̅V̅', 4000], 
+       
+      
       ['M', 1000], ['CM', 900], ['D', 500], ['CD', 400], 
       ['C', 100], ['XC', 90], ['L', 50], ['XL', 40], 
       ['X', 10], ['IX', 9], ['V', 5], ['IV', 4], 
@@ -26,9 +26,9 @@ function convertToRoman(num) {
 // Example Test Cases
 console.log(convertToRoman(14));       // Output: "XIV"
 console.log(convertToRoman(798));      // Output: "DCCXCVIII"
-console.log(convertToRoman(3999));     // Output: "MMMCMXCIX"
-console.log(convertToRoman(10000));    // Output: "X̅"
-console.log(convertToRoman(50000));    // Output: "L̅"
-console.log(convertToRoman(99999));    // Output: "C̅M̅X̅C̅I̅X̅"
+console.log(convertToRoman(36));     // Output: "MMMCMXCIX"
+console.log(convertToRoman(0));    // Output: "X̅"
+console.log(convertToRoman(3999));    // Output: "L̅"
+console.log(convertToRoman(100000));    // Output: "C̅M̅X̅C̅I̅X̅"
 
 module.exports=convertToRoman;
