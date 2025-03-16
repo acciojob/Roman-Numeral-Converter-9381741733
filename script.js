0af6f08ab76f90c1ad7a56701073438d683ed108
jyothsna bhukya <jyothsnabhukya022@gmail.com>
2025-03-16 11:06:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,16 @@
 function convertToRoman(num) {
     let romanSymbols = [
-        ["M", 1000],
-        ["CM", 900],
-        ["D", 500],
-        ["CD", 400],
-        ["C", 100],
-        ["XC", 90],
-        ["L", 50],
-        ["XL", 40],
-        ["X", 10],
-        ["IX", 9],
-        ["V", 5],
-        ["IV", 4],
-        ["I", 1]
+        ["M", 1000], ["CM", 900], ["D", 500], ["CD", 400],
+        ["C", 100], ["XC", 90], ["L", 50], ["XL", 40],
+        ["X", 10], ["IX", 9], ["V", 5], ["IV", 4], ["I", 1]
     ];
 
     let result = "";
-    
-    for (let [symbol, value] of romanSymbols) {
-        while (num >= value) {
-            result += symbol;
-            num -= value;
+
+    for (let i = 0; i < romanSymbols.length; i++) {
+        while (num >= romanSymbols[i][1]) {
+            result += romanSymbols[i][0];
+            num -= romanSymbols[i][1];
         }
     }
 
